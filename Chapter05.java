e8cf7f5d4d44e3689409afee382304f553879c95
NdaToshitaka <93751447+NodaToshitaka@users.noreply.github.com>
2022-03-16 21:00:23 +0900

    1

---
+++ Chapter05.java
@@ -0,0 +1,24 @@
+public class Chapter05 {
+  public static void main(String[] args) {
+    long lo1 = 100000000000000L;
+    System.out.println(lo1);
+
+    double do1 = 3.141421356;
+    System.out.println(do1);
+
+    boolean bo1 = true;
+    System.out.println(bo1);
+    
+    Integer in1 = 100;
+    System.out.println(in1);
+
+    Float fl1 = 1.4142f;
+    System.out.println(fl1);
+
+    Character ch1 = 'A';
+    System.out.println(ch1);
+
+    int[] array1 = {10, 20, 30, 40, 50};
+    System.out.println(array1[3]);
+  }
+}
---
+++ Variable02.java
@@ -0,0 +1,13 @@
+public class Variable02 {
+  public static void main(String[] args) {
+    String str1;
+    str1 = "Noda Toshitaka";
+    System.out.println(str1);
+
+    str1 = "never give up!";
+    System.out.println(str1);
+
+    int num1 = 99;
+    System.out.println(num1);
+  }
+}
