708aa6e97199a6bd7d73d90b4421e4151eabe118
NdaToshitaka <93751447+NodaToshitaka@users.noreply.github.com>
2022-03-24 19:57:12 +0900

    9

--- JavaWcp/src/practice/Chapter15_1.java
+++ JavaWcp/src/practice/Chapter15_1.java
@@ -4,7 +4,10 @@ public class Chapter15_1 {
 	public static void main(String[] args) {
         int a = 976;
         int b = 427;
+        int c = 777;
+        int d = 444;
         System.out.println(a + " と " + b + " の最大公約数は、" + gcd(a, b) + "です。");
+        System.out.println(c + " と " + d + " の最大公約数は、" + gcd(c, d) + "です。");
     }
 
     static int gcd(int a, int b) {
