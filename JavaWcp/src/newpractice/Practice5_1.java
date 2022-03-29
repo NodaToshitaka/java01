f70efff1a36b5a7d453c02387082ce4cfad88149
NdaToshitaka <93751447+NodaToshitaka@users.noreply.github.com>
2022-03-29 16:00:25 +0900

    14

---
+++ JavaWcp/src/newpractice/Practice5_1.java
@@ -0,0 +1,11 @@
+package newpractice;
+
+public class Practice5_1 {
+	public static void introductOnself(String a, int b, double c, char d) {
+		System.out.println("名前は" + a + "年齢は" + b + "身長は" + c + "干支は" + d + "です");
+	}
+	public static void main(String[] args) {
+		introductOnself("高橋健司", 34, 185.2, '寅');
+	}
+
+}
