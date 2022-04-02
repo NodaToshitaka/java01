6a71e19a3e0153d6ddd621ff936583fa4129a277
NdaToshitaka <93751447+NodaToshitaka@users.noreply.github.com>
2022-04-02 18:05:51 +0900

    18

---
+++ JavaWcp/src/newpractice/Clelic.java
@@ -0,0 +1,28 @@
+package newpractice;
+
+public class Clelic {
+	String name;
+	
+	final int HP = 50;
+	int hp = HP;
+	
+	final int MP = 10;
+	int mp = MP;
+	
+	public void selfAid() {
+		System.out.println(this.name + "はセルフエイドを唱えた");
+		System.out.println("MPを５消費、全回復した");
+		this.mp -= 5;
+		this.hp = HP;
+	}
+	
+	public int pray(int sec) {
+		System.out.println(this.name + "は" + sec + "秒間祈った");
+		int mpAid = new java.util.Random().nextInt(3) + sec;
+		int returnMp = Math.min(this.MP - this.mp, mpAid);
+		System.out.println(returnMp + "ポイントMPが回復した");
+		
+		return returnMp;
+	}
+
+}
