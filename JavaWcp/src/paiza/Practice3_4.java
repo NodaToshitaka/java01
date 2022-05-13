4cc7d4674fa64778e8baaea385a6228895cd95fc
NdaToshitaka <93751447+NodaToshitaka@users.noreply.github.com>
2022-05-14 03:27:46 +0900

    59

---
+++ JavaWcp/src/paiza/Practice3_4.java
@@ -0,0 +1,15 @@
+package paiza;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Practice3_4 {
+	public static void main(String[] args) {
+	       
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        BigInteger n = new BigInteger(line);
+        System.out.println(n);
+    }
+
+}
